7cbe3d09d0b4eff4294c9f87190a5fc0e53d5b3b
White Buffalo <apothocare@gmail.com>
2023-03-12 01:51:49 -0600

    Added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from 'astro/config';
 import sanity from "astro-sanity";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [sanity()]
+  integrations: [sanity()],
+  output: "server",
+  adapter: vercel()
 });
