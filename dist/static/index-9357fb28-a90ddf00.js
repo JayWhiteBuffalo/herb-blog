906072357d5da4c40a0f23f6d4b9f68b45a667c6
White Buffalo <apothocare@gmail.com>
2023-03-20 23:39:36 -0500

    Changed export const to export default in ./studio/schemas/index in attempt to resolve build errors. Works locally

---
+++ dist/static/index-9357fb28-a90ddf00.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-6c71f0c1-2467e224.js";import"./sanity-e7dac0b4.js";import"./index-67540257.js";export{m as DocumentPaneProvider,a as default};
---
+++ dist/static/index-9357fb28-b5286c53.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-6c71f0c1-87e1256a.js";import"./sanity-42163f22.js";import"./index-f015cd07.js";export{m as DocumentPaneProvider,a as default};
